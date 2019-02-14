45244ddce7cb628be1e2742201579be4240b11e9
CMSQATeam <43958307+CMSQATeam@users.noreply.github.com>
2019-02-14 19:19:40 +0530

    Create ClassProperty.cs

---
+++ ScriptRef/ClassProperty.cs
@@ -0,0 +1,11 @@
+namespace ScriptRef
+{
+    public class ClassProperty
+    {
+        public int NumberA { get; set; }
+
+        public int NumberB { get; set; }
+
+        public int NumberC { get; set; }
+    }
+}
